dc3c77f65d7a06582c75fceb3f6635fd20cee210
luluci <lunacied@gmail.com>
2023-05-01 21:45:57 +0900

    Initial commit

---
+++ src-tauri/src/handler.rs
@@ -0,0 +1,10 @@
+
+use os;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+pub fn message_box(msg: &str) {
+    os::message_box(msg);
+    //format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,26 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+mod handler;
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app|{
+            //let _window = tauri::WindowBuilder::new(app, "label", tauri::WindowUrl::App("index.html".into())).build()?;
+            let main_window = tauri::Manager::get_window(app, "main").unwrap();
+            main_window.eval(&format!("window.location.replace('{}')", tauri::WindowUrl::App("test.html".into()))).unwrap();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            handler::message_box,
+            greet,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
